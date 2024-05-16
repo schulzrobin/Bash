a4ead5fcfe4c080bdcfe8b5f26327e9f42da1476
schulzrobin <106838867+schulzrobin@users.noreply.github.com>
2024-05-16 12:30:34 +0200

    Aktualisieren von post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -30,23 +30,9 @@ if [[ $EUID -ne 0 ]]; then
   			2)
 				  echo "Installing Apache"
 				  apt install apache2 -y
-	            
-    			echo "Installing Mysql Server"
-	 			  apt install mysql-server -y
-
-        	echo "Installing PHP"
-				  apt install php libapache2-mod-php php-mcrypt php-mysql -y
-	            
-        	echo "Installing Phpmyadmin"
-				  apt install phpmyadmin -y
-
-				  echo "Configuring apache to run Phpmyadmin"
-				  echo "Include /etc/phpmyadmin/apache.conf" >> /etc/apache2/apache2.conf
-				
-				  echo "Enabling module rewrite"
-				  sudo a2enmod rewrite
-				  echo "Restarting Apache Server"
-				  service apache2 restart
+	          
+				  echo "Starting Apache Server"
+				  service apache2 start
 				  ;;
         3)	
 				  echo "Installing Build Essentials"
